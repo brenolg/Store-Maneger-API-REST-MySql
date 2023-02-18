e544af804a25d69e82da6bfd1226ca582b25c3f3
Breno Lavalle Garrido <brenolg@yahoo.com.br>
2023-02-18 10:17:21 -0300

    fix: rota post /sales

--- src/models/sale.model.js
+++ src/models/sale.model.js
@@ -1,17 +1,19 @@
 const connection = require('./connection');
 
-const insertSale = async (date) => { 
+const insertSale = async (date) => {
   const [{ insertId }] = await connection.execute(
     'INSERT INTO StoreManager.sales (date) VALUES (?)', [date],
   );
+  
   return insertId;
 };
 
 const insertSaleProduct = async (insertId, sale) => {
-  connection.execute(
+    await connection.execute(
     'INSERT INTO StoreManager.sales_products (sale_id, product_id, quantity) VALUES (?, ?, ?)',
     [insertId, sale.productId, sale.quantity],
-  );
+    
+    );
 };
 
 const findById = async (id) => {
--- src/services/sale.service.js
+++ src/services/sale.service.js
@@ -3,12 +3,9 @@ const { saleModel } = require('../models');
 const createSale = async (sales) => {
   const date = new Date();
   const newId = await saleModel.insertSale(date);
-  const soldItems = await Promise.all(sales.map((sale) => {
-    saleModel.insertSaleProduct(newId, sale); 
-    return sale;
-  }));
-  
- const newMessage = { id: newId, itemsSold: soldItems }; 
+  await Promise.all(sales.map((sale) => saleModel.insertSaleProduct(newId, sale)));
+
+  const newMessage = { id: newId, itemsSold: sales }; 
   
   return { type: null, message: newMessage };
 };
